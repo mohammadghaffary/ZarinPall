aa1eec1da3a1e9c0abd216f387c3ca0b72b512ff
mohammadghaffary <137639454+mohammadghaffary@users.noreply.github.com>
2023-07-03 19:48:19 +0330

    Add files via upload

---
+++ ZarinPall/script.js
@@ -0,0 +1,52 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const crossicon = document.querySelector('.crossicon');
+
+
+hamburgerIcon.addEventListener('click', function () {
+  console.log(hamburgerMenu.style.transform);
+  hamburgerMenu.classList.add('show-hamburger-menu');
+});
+
+crossicon.addEventListener('click', function () {
+  hamburgerMenu.classList.remove('show-hamburger-menu');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showMoreMenu = document.querySelector('#show-more-menu');
+// const showProductMenu = document.querySelector('#show-product-menu');
+// showMoreMenu.addEventListener('mouseenter', function (e) {
+//   const ul = e.target.childNodes[5];
+//   ul.style.animation = 'showUp 0.35s 1';
+//   ul.style.display = 'block';
+// });
+
+// showMoreMenu.addEventListener('mouseleave', function (e) {
+//   const ul = e.target.childNodes[5];
+//   ul.style.display = 'none';
+// });
+
+// showProductMenu.addEventListener('mouseenter', function (e) {
+//   const div = e.target.childNodes[5];
+//   div.style.animation = 'showUp 0.35s 1';
+//   div.style.display = 'flex';
+// });
+
+// showProductMenu.addEventListener('mouseleave', function (e) {
+//   const div = e.target.childNodes[5];
+//   div.style.display = 'none';
+// });
